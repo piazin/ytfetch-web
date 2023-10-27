62d35d31b2323886c7057ef768fc9a2d170cd6b1
Lucas <ls4803326@gmail.com>
2023-10-27 13:04:09 -0300

    :lipstick: feat: added accordion in help section

--- src/components/interface/HelpSection/index.tsx
+++ src/components/interface/HelpSection/index.tsx
@@ -1,15 +1,29 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 import React from "react";
 
 export const HelpSection = () => {
   return (
-    <section className=" text-gray-50 my-12 self-start ml-1">
-      <p>Como posso baixar um vídeo do youtube?</p>
-      <ol className="list-decimal list-inside ml-3 mt-3">
-        <li>Cole o link do vídeo</li>
-        <li>Clique no botão Baixar.</li>
-        <li>Selecione o formato de vídeo desejado.</li>
-        <li>Clique no botão Download.</li>
-      </ol>
+    <section className=" text-gray-50 my-8 self-start ml-1 w-full">
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>
+            Como posso baixar um vídeo do youtube?
+          </AccordionTrigger>
+          <AccordionContent>
+            <ol className="list-decimal list-inside ml-3 mt-3">
+              <li>Cole o link do vídeo</li>
+              <li>Clique no botão Baixar.</li>
+              <li>Selecione o formato de vídeo desejado.</li>
+              <li>Clique no botão Download.</li>
+            </ol>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </section>
   );
 };
